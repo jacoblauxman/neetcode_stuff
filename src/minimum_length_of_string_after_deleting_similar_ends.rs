8571cefa4e68c7115166b36bfe395ac81082d817
Jacob Lauxman <jlauxman@gmail.com>
2024-03-05 10:06:21 -0700

    LC Daily 3/5/24: Minimum Length of String After Deleting Similar Ends

---
+++ src/minimum_length_of_string_after_deleting_similar_ends.rs
@@ -0,0 +1,32 @@
+// # 1750 Minimum Length of String After Deleting Similar Ends
+// note: `daily` for 3/5/24
+
+// ex: `abbacab` -> can take as many from `either` side as long as pre/suffix char matches in both positions
+// -- GREEDY
+// - any time we shift L/R pointers we want to continue the shift until chars no longer match
+// - when do we stop? L + R CANNOT be at same point
+
+// O(n) and O(1)
+
+pub fn minimum_length(s: String) -> i32 {
+    // early return if our string has no way to "match" pre/suffixes
+    if s.len() < 2 {
+        return s.len() as i32;
+    }
+    // define 'L + R' pointers
+    let (mut left, mut right) = (0, s.len() - 1);
+    // `the prefix and suffix should not intersect at any index` + confirming matched chars at ptr pos's
+    while left < right && s.chars().nth(left) == s.chars().nth(right) {
+        // store matched char "temp" for inner iteration
+        let matched_char = s.chars().nth(left);
+        // incr. / decr. "inwards" to center of input String while either ptr pos matches "temp" variable
+        while left <= right && s.chars().nth(left) == matched_char {
+            left += 1;
+        }
+        while right >= left && s.chars().nth(right) == matched_char {
+            right -= 1;
+        }
+    }
+
+    return (right - left) as i32 + 1; // note: `off by one` due to right init. as "len() - 1"
+}
